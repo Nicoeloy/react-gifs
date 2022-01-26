3d23e9998bdc3c27c9c24a50a4072b99a02645a8
Nicolas Eloy colasrsca@gmail.com <colasrsca@gmail.com>
2022-01-26 15:42:55 +0100

    app.jsx file creation & constructor

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+const GIPHY_API_KEY = "BgvgJ7z9KuO0jqm8vEJVKA6AKjIaNTIo"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs = [],
+      selectedGifId = "k8kITi9SAwe9JWbUaH"
+    }
+  }
+}
+
+export default App;
